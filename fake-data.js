76544e3a27c4a101e3e761c22ece88a2b3fcc19d
kolyaratishin <kolyaratishin1@gmail.com>
2023-02-28 11:44:17 +0200

    updated fakedata

--- fake-data.js
+++ fake-data.js
@@ -1,13 +1,17 @@
 module.exports = [{
+    id: "1",
     title: 'Article 1',
     text: "text 1",
 }, {
+    id: "2",
     title: 'Article 2',
     text: "text 2",
 }, {
+    id: "3",
     title: 'Article 3',
     text: "text 3",
 }, {
+    id: "4",
     title: 'Article 4',
     text: "text 4",
 }];
